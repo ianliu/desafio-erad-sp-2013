fe562827025303644e75eaad0ffab302056ba3ed
Ian Liu Rodrigues <ian.liu88@gmail.com>
2013-06-26 14:44:33 -0300

    Fix show.py to show images

--- show.py
+++ show.py
@@ -4,9 +4,18 @@ from PIL import Image
 from struct import unpack
 import sys
 
+
 f = sys.stdin
 cols, rows = unpack('hh', f.read(4))
+img = Image.new('RGBA', (cols, rows))
+
+print img
 
 for row in xrange(rows):
     length = cols * 4
     line = unpack(str(length) + 'c', f.read(length))
+    for col in xrange(cols):
+        color = tuple(map(ord, line[col*4:(col+1)*4]))
+        img.putpixel((col, row), color)
+
+img.show()
