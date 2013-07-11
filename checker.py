ba20d3873e02f4a1f9c469412ef48fdbd3c54159
Ian Liu Rodrigues <ian.liu88@gmail.com>
2013-07-11 11:39:52 -0300

    Add -s option to checker.py

--- checker.py
+++ checker.py
@@ -4,10 +4,15 @@ import sys
 from struct import pack
 
 def usage():
-    sys.stderr.write("Usage: %s -w WIDTH -h HEIGHT\n" % (sys.argv[0], ))
+    sys.stderr.write("Usage: %s -w WIDTH -h HEIGHT [-s SIZE]\n" % \
+            (sys.argv[0], ))
+    sys.stderr.write("      -w  WIDTH  Image width\n")
+    sys.stderr.write("      -h  HEIGHT Image height\n")
+    sys.stderr.write("      -s  SIZE   Size of the checkers (default 100)\n")
     sys.exit(1)
 
 height, width = 0, 0
+size = 100
 
 i = 1
 while i < len(sys.argv):
@@ -17,6 +22,9 @@ while i < len(sys.argv):
     elif sys.argv[i] == "-h":
         height = int(sys.argv[i+1])
         i += 1
+    elif sys.argv[i] == "-s":
+        size = int(sys.argv[i+1])
+        i += 1
     i += 1
 
 if height == 0 or width == 0:
@@ -25,7 +33,6 @@ if height == 0 or width == 0:
 f = sys.stdout
 f.write(pack('hh', width, height))
 
-size = 100
 color = [(chr(255), chr(  0), chr(  0), chr(255)),
          (chr(  0), chr(255), chr(  0), chr(255)),
          (chr(  0), chr(  0), chr(255), chr(255))]
