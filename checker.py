09b6aa5bafe34ac54297ce1c45905121cea0b1e2
Ian Liu Rodrigues <ian.liu88@gmail.com>
2013-07-11 11:39:24 -0300

    Use "width" and "height" instead of "rows" and "cols"

--- checker.py
+++ checker.py
@@ -7,30 +7,30 @@ def usage():
     sys.stderr.write("Usage: %s -w WIDTH -h HEIGHT\n" % (sys.argv[0], ))
     sys.exit(1)
 
-rows, cols = 0, 0
+height, width = 0, 0
 
 i = 1
 while i < len(sys.argv):
     if sys.argv[i] == "-w":
-        cols = int(sys.argv[i+1])
+        width = int(sys.argv[i+1])
         i += 1
     elif sys.argv[i] == "-h":
-        rows = int(sys.argv[i+1])
+        height = int(sys.argv[i+1])
         i += 1
     i += 1
 
-if rows == 0 or cols == 0:
+if height == 0 or width == 0:
     usage()
 
 f = sys.stdout
-f.write(pack('hh', cols, rows))
+f.write(pack('hh', width, height))
 
 size = 100
 color = [(chr(255), chr(  0), chr(  0), chr(255)),
          (chr(  0), chr(255), chr(  0), chr(255)),
          (chr(  0), chr(  0), chr(255), chr(255))]
 
-for row in xrange(rows):
-    for col in xrange(cols):
+for row in xrange(height):
+    for col in xrange(width):
         c = (row/size + col/size) % 3
         f.write(pack('cccc', *color[c]))
--- show.py
+++ show.py
@@ -6,15 +6,15 @@ import sys
 
 
 f = sys.stdin
-cols, rows = unpack('hh', f.read(4))
-img = Image.new('RGBA', (cols, rows))
+width, height = unpack('hh', f.read(4))
+img = Image.new('RGBA', (width, height))
 
-print cols, rows
+print width, height
 
-for row in xrange(rows):
-    length = cols * 4
+for row in xrange(height):
+    length = width * 4
     line = unpack(str(length) + 'c', f.read(length))
-    for col in xrange(cols):
+    for col in xrange(width):
         color = tuple(map(ord, line[col*4:(col+1)*4]))
         img.putpixel((col, row), color)
 
