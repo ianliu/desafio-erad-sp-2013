024c11a89cda0b7eff74d148fd189ac0013175f4
Ian Liu Rodrigues <ian.liu88@gmail.com>
2013-07-25 16:18:06 -0300

    Smooth several windows at once

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 #define MAX_H 4320
 
 /* Tamanho da borda de zeros na imagem para um smooth de janela 5x5 */
-#define BORDER 2
+#define BORDER 4
 
 /* Tamanho máximo de uma imagem */
 #define MAX_SIZE ((MAX_W+2*BORDER)*(MAX_H+2*BORDER)*4)
@@ -35,7 +35,7 @@
 } while(0)
 #endif
 
-#define NUM_THREADS 1
+#define NUM_THREADS 2
 
 static uint16_t width, height;
 static uint8_t img[MAX_SIZE] = {0,};
@@ -51,44 +51,79 @@ void *smooth5_slice(void *data)
 {
 	uintptr_t tid = (uintptr_t)data;
 	int w = width + 2*BORDER;
-	int i, j, k, x0, x1, y0, y1;
+	int i, j, k, l, x0, x1, y0, y1;
 	x0 = BORDER + slices[tid].x0;
 	x1 = BORDER + slices[tid].x1;
 	y0 = BORDER + slices[tid].y0;
 	y1 = BORDER + slices[tid].y1;
 	for (i = y0; i < y1; i++) {
-		for (j = x0; j < x1; j++) {
+		for (j = x0; j < x1; j += 16) {
 			int idx;
-			__m128i zero = _mm_setzero_si128();
 			__m128i line;
-			__m128i sum = _mm_setzero_si128();
-			/* __m128i s1 = _mm_setzero_si128(); */
-			/* __m128i s2 = _mm_setzero_si128(); */
-			/* __m128i s3 = _mm_setzero_si128(); */
-			/* __m128i s4 = _mm_setzero_si128(); */
-			/* __m128i s5 = _mm_setzero_si128(); */
+			__m128i zero = _mm_setzero_si128();
+			__m128i s0 = _mm_setzero_si128();
+			__m128i s1 = _mm_setzero_si128();
+			__m128i s2 = _mm_setzero_si128();
+			__m128i s3 = _mm_setzero_si128();
+			__m128i s4 = _mm_setzero_si128();
+			__m128i s5 = _mm_setzero_si128();
+			__m128i s6 = _mm_setzero_si128();
+			__m128i s7 = _mm_setzero_si128();
+			__m128i s8 = _mm_setzero_si128();
+			__m128i s9 = _mm_setzero_si128();
 
 			for (k = i-2; k <= i+2; k++) {
 				idx = (k*w + j-2)*4;
 				line = _mm_loadu_si128((const __m128i *)&img[idx]);
-				sum = _mm_add_epi16(sum, _mm_unpackhi_epi8(line, zero));
-				sum = _mm_add_epi16(sum, _mm_unpacklo_epi8(line, zero));
+				s0 = _mm_add_epi16(s0, _mm_unpacklo_epi8(line, zero));
+				s1 = _mm_add_epi16(s1, _mm_unpackhi_epi8(line, zero));
+
+				idx += 16;
+				line = _mm_loadu_si128((const __m128i *)&img[idx]);
+				s2 = _mm_add_epi16(s2, _mm_unpacklo_epi8(line, zero));
+				s3 = _mm_add_epi16(s3, _mm_unpackhi_epi8(line, zero));
+
+				idx += 16;
+				line = _mm_loadu_si128((const __m128i *)&img[idx]);
+				s4 = _mm_add_epi16(s4, _mm_unpacklo_epi8(line, zero));
+				s5 = _mm_add_epi16(s5, _mm_unpackhi_epi8(line, zero));
+
+				idx += 16;
+				line = _mm_loadu_si128((const __m128i *)&img[idx]);
+				s6 = _mm_add_epi16(s6, _mm_unpacklo_epi8(line, zero));
+				s7 = _mm_add_epi16(s7, _mm_unpackhi_epi8(line, zero));
+
+				idx += 16;
+				line = _mm_loadu_si128((const __m128i *)&img[idx]);
+				s8 = _mm_add_epi16(s8, _mm_unpacklo_epi8(line, zero));
+				s9 = _mm_add_epi16(s9, _mm_unpackhi_epi8(line, zero));
 			}
 
-			uint64_t pixels[2];
-			_mm_storeu_si128((__m128i *)&pixels[0], sum);
-			uint64_t s = pixels[0] + pixels[1];
+			uint64_t pixels[21];
+			uint64_t s = 0;
 
-			/* The masks are inverted here because Intel uses little endian */
-			for (k = i-2; k <= i+2; k++) {
-				int idx = (k*w + j+2)*4;
-				s += ((uint64_t)img[idx+0]) | ((uint64_t)img[idx+1] << 16) | ((uint64_t)img[idx+2] << 32) | ((uint64_t)img[idx+3] << 48);
+			_mm_storeu_si128((__m128i *)&pixels[0], s0);
+			_mm_storeu_si128((__m128i *)&pixels[2], s1);
+			_mm_storeu_si128((__m128i *)&pixels[4], s2);
+			_mm_storeu_si128((__m128i *)&pixels[6], s3);
+			_mm_storeu_si128((__m128i *)&pixels[8], s4);
+			_mm_storeu_si128((__m128i *)&pixels[10], s5);
+			_mm_storeu_si128((__m128i *)&pixels[12], s6);
+			_mm_storeu_si128((__m128i *)&pixels[14], s7);
+			_mm_storeu_si128((__m128i *)&pixels[16], s8);
+			_mm_storeu_si128((__m128i *)&pixels[18], s9);
+
+			for (l = 0; l < 5; l++)
+				s += pixels[l];
+
+			for (l = j; l < j+16; l++) {
+				out[(i*w + l)*4 + 0] = ((s & 0x000000000000ffff)) / 25;
+				out[(i*w + l)*4 + 1] = ((s & 0x00000000ffff0000) >> 16) / 25;
+				out[(i*w + l)*4 + 2] = ((s & 0x0000ffff00000000) >> 32) / 25;
+				out[(i*w + l)*4 + 3] = ((s & 0xffff000000000000) >> 48) / 25;
+				s -= pixels[l-j];
+				s += pixels[l-j+5];
 			}
-
-			out[(i*w + j)*4 + 0] = ((s & 0x000000000000ffff)) / 25;
-			out[(i*w + j)*4 + 1] = ((s & 0x00000000ffff0000) >> 16) / 25;
-			out[(i*w + j)*4 + 2] = ((s & 0x0000ffff00000000) >> 32) / 25;
-			out[(i*w + j)*4 + 3] = ((s & 0xffff000000000000) >> 48) / 25;
 		}
 	}
 	return NULL;
